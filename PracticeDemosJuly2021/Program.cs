e0ef04b97580593023ee04ef7a2df7f10206a24d
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-07-15 19:59:52 +0100

    Add project files.

---
+++ PracticeDemosJuly2021/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PracticeDemosJuly2021
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PracticeDemosJuly2021/RomanNumeralConversion/RomanNumeralConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeDemosJuly2021.RomanNumeralConversion
+{
+    public class RomanNumeralConverter
+    {
+        private RomanNumeralLookup romanNumeralLookup;
+        public RomanNumeralConverter()
+        {
+            romanNumeralLookup = new RomanNumeralLookup();
+        }
+
+        public string Numerals(int num)
+        {
+            string result = string.Empty;
+            // 
+            return result;
+        }
+
+    }
+
+    internal class RomanNumeralLookup
+    {
+        Dictionary<int, string> numeralLookup;
+        public RomanNumeralLookup()
+        {
+            numeralLookup = new Dictionary<int, string>()
+            {
+                { 1,"I" },
+                { 5,"V" },
+                { 10,"X" },
+                { 50,"L" },
+                { 100,"C" },
+                { 500,"D" },
+                { 1000,"M" },
+            };
+        }
+
+        public string GetRomanNumeral(int key) {
+            return numeralLookup.GetValueOrDefault(key);
+        }
+    }
+}
