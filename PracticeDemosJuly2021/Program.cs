050628ab59d5fb7b5ae6f95f2217c9e16ee1d65f
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-07-15 22:04:08 +0100

    add conditions and methods to transform number to tens and hundreds

--- PracticeDemosJuly2021/Program.cs
+++ PracticeDemosJuly2021/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PracticeDemosJuly2021.RomanNumeralConversion;
+using System;
 
 namespace PracticeDemosJuly2021
 {
@@ -7,7 +8,32 @@ namespace PracticeDemosJuly2021
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Pratice tests");
+            convertToRomanNumeral();
+        }
+
+        static void convertToRomanNumeral()
+        {
+            Console.WriteLine("Convert to Roman Symbols");
+            RomanNumeralConverter converter = new RomanNumeralConverter();
+            //Console.WriteLine($"result -: {converter.Numerals(5000)}");
+            //Console.WriteLine($"result -: {converter.Numerals(1)}");
+            //Console.WriteLine($"result -: {converter.Numerals(2)}");
+            //Console.WriteLine($"result -: {converter.Numerals(3)}");
+            //Console.WriteLine($"result -: {converter.Numerals(4)}");
+            //Console.WriteLine($"result -: {converter.Numerals(5)}");
+            //Console.WriteLine($"result -: {converter.Numerals(6)}");
+            //Console.WriteLine($"result -: {converter.Numerals(7)}");
+            Console.WriteLine($"result -: {converter.Numerals(78)}");
+            Console.WriteLine($"result -: {converter.Numerals(59)}");
+            Console.WriteLine($"result -: {converter.Numerals(24)}");
+            Console.ReadKey();
+
+        }
+
+        static void PrintResult()
+        {
+
         }
     }
 }
--- PracticeDemosJuly2021/RomanNumeralConversion/RomanNumeralConverter.cs
+++ PracticeDemosJuly2021/RomanNumeralConversion/RomanNumeralConverter.cs
@@ -21,17 +21,112 @@ namespace PracticeDemosJuly2021.RomanNumeralConversion
             if (IsValidNumeralNumber(num))
             {
                 //process numeral digits
+                string numSequence = num.ToString();
+                int numLength = numSequence.Length;
+                foreach(var n in numSequence)
+                {
+                    // Console.WriteLine($" {n} {numLength}");
+                    int currentNumPlace = Convert.ToInt32(n.ToString());
+                    result += ProcessNumberSequenceItem(currentNumPlace, numLength);                    
+                    numLength--;
+                }
             }
             else
             {
                 result = "The number entered cannot be expressed as a roman numeral";
             }
             // 
-            return result;
+            return $"{ num } => {result}";
         }
         private bool IsValidNumeralNumber(int num)
         {
             return num > 0 && num < 4000; 
+        }
+
+        private string GetRepeatedSequence(int key, int count)
+        {
+            return string.Join("", Enumerable.Repeat(romanNumeralLookup.GetRomanNumeral(key), count));
+        }
+
+        private string ProcessNumberSequenceItem(int num, int pos)
+        {
+            if (num < 4)
+            {
+                switch (pos)
+                {
+                    case 1:
+                        return GetRepeatedSequence(1, num);                        
+                    case 2:
+                        return GetRepeatedSequence(10, num);
+                    case 3:
+                        return GetRepeatedSequence(100, num);
+                    default:
+                        return string.Empty;
+                }
+                
+            }
+            else if (num == 4)
+            {
+                switch (pos)
+                {
+                    case 1:
+                        return romanNumeralLookup.GetRomanNumeral(1) + romanNumeralLookup.GetRomanNumeral(5);
+                    case 2:
+                        return romanNumeralLookup.GetRomanNumeral(10) + romanNumeralLookup.GetRomanNumeral(50);
+                    case 3:
+                        return romanNumeralLookup.GetRomanNumeral(100) + romanNumeralLookup.GetRomanNumeral(500);
+                    default:
+                        return string.Empty;
+                }
+                
+            }
+            else if (num == 5)
+            {
+                switch (pos)
+                {
+                    case 1:
+                        return romanNumeralLookup.GetRomanNumeral(5);
+                    case 2:
+                        return romanNumeralLookup.GetRomanNumeral(50);
+                    case 3:
+                        return romanNumeralLookup.GetRomanNumeral(500);
+                    default:
+                        return string.Empty;
+                }
+                
+            }
+            else if (num > 5 && num < 9)
+            {
+                switch (pos)
+                {
+                    case 1:
+                        return romanNumeralLookup.GetRomanNumeral(5) + GetRepeatedSequence(1, num - 5);
+                    case 2:
+                        return romanNumeralLookup.GetRomanNumeral(50) + GetRepeatedSequence(10, num - 5); ;
+                    case 3:
+                        return romanNumeralLookup.GetRomanNumeral(500) + GetRepeatedSequence(100, num - 5); ;
+                    default:
+                        return string.Empty;
+                }                
+            }
+            else if (num == 9)
+            {
+                switch (pos)
+                {
+                    case 1:
+                        return romanNumeralLookup.GetRomanNumeral(1) + romanNumeralLookup.GetRomanNumeral(10);
+                    case 2:
+                        return romanNumeralLookup.GetRomanNumeral(10) + romanNumeralLookup.GetRomanNumeral(100);
+                    case 3:
+                        return romanNumeralLookup.GetRomanNumeral(100) + romanNumeralLookup.GetRomanNumeral(1000);
+                    default:
+                        return string.Empty;
+                }                
+            }
+            else
+            {
+                return "";
+            }
         }
 
     }
